e3e3cc4c4bef3b3df40c09467800b0cb914d8a53
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 20:00:40 +0200

    test heroku

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { IUser } from './interfaces';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3049;
 
-interface IUser {
-	firstName: string;
-	lastName: string;
-	accessGroups: string[];
-}
-
 const user: IUser = {
 	firstName: "Hendrick",
-	lastName: "Denzmann",
+	lastName: "Denzmann-changed",
 	accessGroups: ['loggedInUsers', 'members']
 };
 
