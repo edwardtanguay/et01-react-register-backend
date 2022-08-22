01bb2416bde6cf349584ad9f702fbf2542c32787
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 22:28:19 +0200

    session-cookies in

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { getUsers } from './models.js';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3049;
 const users = getUsers();
+
+app.use(
+	session({
+		resave: true,
+		saveUninitialized: true,
+		secret: 'tempsecret'
+
+	})
+);
+
+app.use(cookieParser());
 
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send(users);
