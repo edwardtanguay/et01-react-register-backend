f38675fd0d67eb389e8cdeea059bd4011a321c7c
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-22 21:51:56 +0200

    added username

--- src/models.ts
+++ src/models.ts
@@ -3,11 +3,13 @@ import { IUser } from './interfaces.js';
 export const getUsers = (): IUser[] => {
 	return [
 		{
+			username: 'anonymousUser',
 			firstName: "Anonymous",
 			lastName: "User",
 			accessGroups: ['loggedOutUsers']
 		},
 		{
+			username: 'hd',
 			firstName: "Hendrick",
 			lastName: "Denzmann",
 			accessGroups: ['loggedInUsers', 'members']
